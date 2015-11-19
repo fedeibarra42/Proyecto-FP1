8e519bd2bdad6e723bf3b7d28e5f4dcdd3a17494
fedeibarra42 <fedeibarra42@gmail.com>
2015-11-19 14:02:48 -0600

    Update deportes.h

--- deportes.h
+++ deportes.h
@@ -2,9 +2,14 @@ float baseball_menu(dinero){
 	char equipos[40][15] = {{"Giants"},{"Dodgers"},{"Padres"},{"Roockies"},{"Dimodbacks"},{"Blue Jays"},{"Orioles"},{"Rays"},{"Red Sox"},{"Yankees"},
 				{"Royals"},{"Twins"},{"Indians"},{"White Sox"},{"Tigers"},{"Rangers"},{"Astros"},{"Angels"},{"Marineers"},{"Athletics"},{"Mets"},{"Nationals"},
 				{"Marlins"},{"Braves"},{"Phillies"},{"Cardinals"},{"Pirates"},{"Cubs"},{"Brewers"},{"Reds"}};
-	int /*orden[40],*/c;
+	int c;
+	
+	//Es el numero de juegos que gano cada equipo en la pasada temporada.
 	float stats[40] = {84,92,74,68,79,93,87,81,80,78,95,83,81,76,74,88,86,85,76,68,90,83,71,67,63,100,98,97,68,64};
 
+	/*Divide cada numero del arreglo stats entre 162 que fue la cantidad de juegos que hubo en la temporada para generar 
+	  el promedio de juegos ganados de cada equipo.
+	*/
 	for (c=0;c<30;c++)
 		stats[c]/=162;
 
