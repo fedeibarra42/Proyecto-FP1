516fea6ccac9ea95c07b8586812d53b92d7741bb
fedeibarra42 <fedeibarra42@gmail.com>
2015-11-22 18:37:13 -0600

    Create ayuda.h

---
+++ ayuda.h
@@ -0,0 +1,7 @@
+void ayuda(){
+	printf("Ayuda\n");
+	printf("Instrucciones:\n");
+	printf("Para empezar el juego presionar ""2"" para crear un usuaro,\nsi ya tienes uno presiona ""1"". El juego empezará con $0, si se quiere agregar dinero seleciona\nla opcion de depositar.\n");
+	printf("\nPara apostar eleciona la opcion 3 en el menú de usuario,\ny elige al deporte en el que quieres apostar;\nya que elegiste el deporte aparecerán los distitntos partidos que hay,\nal lado de cada equipo hay un numero para identificar cada\nequipo.\nElige el equipo al que quieras apostar y aparecerán los resultados.\nSi ganaste el programa hará los cálculos del dinero que ganaste.\n");
+	printf("\n\nDesarrolladores:\nFederico Ibarra\nLeón Arpio\nÁlvaro Cabrales\n");
+}
